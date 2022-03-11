24f51a6cadf60356dd2777339d6dda9d0dde3a7f
Luivatra <luivatra@gmail.com>
2022-03-11 15:47:31 +0100

    added negative test cases nftlockedvesting

--- app/test/test_vesting.py
+++ app/test/test_vesting.py
@@ -1,6 +1,7 @@
 import pytest
 from config import Config, Network
 from ergo.appkit import ErgoAppKit, ErgoValueT
+from sigmastate.lang.exceptions import InterpreterException
 
 
 CFG = Config[Network]
@@ -17,6 +18,7 @@ class TestNFTLockedVesting:
 
     vestedTokenId = '81ba2a45d4539045995ad6ceeecf9f14b942f944a1c9771430a89c3f88ee898a'
     vestingKey = 'f9e5ce5aa0d95f5d54a7bc89c46730d9662397067250aa18a0039631c0f5b809'
+    fakeVestingKey = 'f9e5ce5aa0d95f5d54a7bc89c46730d9662397067250aa18a0039631c0f5b808'
 
     duration = int(1000*60*60*24)
 
@@ -78,7 +80,7 @@ class TestNFTLockedVesting:
         )
         signed = False
         try:
-            signedTx = self.appKit.signTransaction(unsignedTx)
+            self.appKit.signTransaction(unsignedTx)
             signed = True
         except Exception as e:
             signed = False
@@ -109,10 +111,136 @@ class TestNFTLockedVesting:
         )
         signed = False
         try:
-            signedTx = self.appKit.signTransaction(unsignedTx)
+            self.appKit.signTransaction(unsignedTx)
             signed = True
         except Exception as e:
             print(f"Error: {e}")
             signed = False
     
         assert signed
+
+    def test_redeem_too_much(self):          
+        userInputBox = self.appKit.buildInputBox(int(2e6), 
+            {
+                self.vestingKey: 1
+            }, 
+            registers=None, contract=self.appKit.dummyContract())
+
+        #Set the preheader to 2.5 days after vesting start, so 2*redeem amount should be free to claim
+        preHeader = self.appKit.preHeader(timestamp=int(1648771200000+self.duration*2.5))
+
+        newVestingBox = self.appKit.buildOutBox(self.vestingInputBox.getValue(), {
+                self.vestedTokenId: int(999999-int(999999/365)*3)
+            },
+            [
+            #Vesting key
+            self.appKit.ergoValue(self.vestingKey, ErgoValueT.ByteArrayFromHex),  
+            #Redeem period
+            self.appKit.ergoValue(self.duration,ErgoValueT.Long),    
+            #Redeem amount per period              
+            self.appKit.ergoValue(int(999999/365),ErgoValueT.Long),     
+            #Start vesting april 1st                      
+            self.appKit.ergoValue(int(1648771200000),ErgoValueT.Long),    
+            #Total vested                   
+            self.appKit.ergoValue(int(999999),ErgoValueT.Long)                               
+            ], self.contract)
+
+        newUserBox = self.appKit.buildOutBox(int(1e6), {
+            self.vestingKey: 1,
+            self.vestedTokenId: int(999999/365)*3
+            }, registers=None, contract=self.appKit.dummyContract())
+
+        unsignedTx = self.appKit.buildUnsignedTransaction(
+            inputs = [self.vestingInputBox, userInputBox],
+            outputs = [newVestingBox,newUserBox],
+            fee = int(1e6),
+            sendChangeTo = self.appKit.dummyContract().getAddress().getErgoAddress(),
+            preHeader = preHeader
+        )
+
+        with pytest.raises(InterpreterException):
+            self.appKit.signTransaction(unsignedTx)
+
+    def test_wrong_key_redeem(self):          
+        userInputBox = self.appKit.buildInputBox(int(2e6), 
+            {
+                self.fakeVestingKey: 1
+            }, 
+            registers=None, contract=self.appKit.dummyContract())
+
+        #Set the preheader to 2.5 days after vesting start, so 2*redeem amount should be free to claim
+        preHeader = self.appKit.preHeader(timestamp=int(1648771200000+self.duration*2.5))
+
+        newVestingBox = self.appKit.buildOutBox(self.vestingInputBox.getValue(), {
+                self.vestedTokenId: int(999999-int(999999/365)*2)
+            },
+            [
+            #Vesting key
+            self.appKit.ergoValue(self.vestingKey, ErgoValueT.ByteArrayFromHex),  
+            #Redeem period
+            self.appKit.ergoValue(self.duration,ErgoValueT.Long),    
+            #Redeem amount per period              
+            self.appKit.ergoValue(int(999999/365),ErgoValueT.Long),     
+            #Start vesting april 1st                      
+            self.appKit.ergoValue(int(1648771200000),ErgoValueT.Long),    
+            #Total vested                   
+            self.appKit.ergoValue(int(999999),ErgoValueT.Long)                               
+            ], self.contract)
+
+        newUserBox = self.appKit.buildOutBox(int(1e6), {
+            self.fakeVestingKey: 1,
+            self.vestedTokenId: int(999999/365)*2
+            }, registers=None, contract=self.appKit.dummyContract())
+
+        unsignedTx = self.appKit.buildUnsignedTransaction(
+            inputs = [self.vestingInputBox, userInputBox],
+            outputs = [newVestingBox,newUserBox],
+            fee = int(1e6),
+            sendChangeTo = self.appKit.dummyContract().getAddress().getErgoAddress(),
+            preHeader = preHeader
+        )
+        with pytest.raises(InterpreterException):
+            self.appKit.signTransaction(unsignedTx)
+
+    def test_redeem_not_enough(self):          
+        userInputBox = self.appKit.buildInputBox(int(2e6), 
+            {
+                self.vestingKey: 1
+            }, 
+            registers=None, contract=self.appKit.dummyContract())
+
+        #Set the preheader to 2.5 days after vesting start, so 2*redeem amount should be free to claim
+        preHeader = self.appKit.preHeader(timestamp=int(1648771200000+self.duration*2.5))
+
+        newVestingBox = self.appKit.buildOutBox(self.vestingInputBox.getValue(), {
+                self.vestedTokenId: int(999999-int(999999/365)*1)
+            },
+            [
+            #Vesting key
+            self.appKit.ergoValue(self.vestingKey, ErgoValueT.ByteArrayFromHex),  
+            #Redeem period
+            self.appKit.ergoValue(self.duration,ErgoValueT.Long),    
+            #Redeem amount per period              
+            self.appKit.ergoValue(int(999999/365),ErgoValueT.Long),     
+            #Start vesting april 1st                      
+            self.appKit.ergoValue(int(1648771200000),ErgoValueT.Long),    
+            #Total vested                   
+            self.appKit.ergoValue(int(999999),ErgoValueT.Long)                               
+            ], self.contract)
+
+        newUserBox = self.appKit.buildOutBox(int(1e6), {
+            self.vestingKey: 1,
+            self.vestedTokenId: int(999999/365)*1
+            }, registers=None, contract=self.appKit.dummyContract())
+
+        unsignedTx = self.appKit.buildUnsignedTransaction(
+            inputs = [self.vestingInputBox, userInputBox],
+            outputs = [newVestingBox,newUserBox],
+            fee = int(1e6),
+            sendChangeTo = self.appKit.dummyContract().getAddress().getErgoAddress(),
+            preHeader = preHeader
+        )
+
+        with pytest.raises(InterpreterException):
+            self.appKit.signTransaction(unsignedTx)
+
