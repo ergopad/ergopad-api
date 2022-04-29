2994e286e136603267bdb08975b8575e0a64267c
Viking Phoenix Consulting <91683182+vikingphoenix@users.noreply.github.com>
2022-04-29 02:05:39 -0600

    Revert "Bot enabled vesting"

--- app/api/v1/routes/blockchain.py
+++ app/api/v1/routes/blockchain.py
@@ -803,7 +803,7 @@ async def airdrop(
     if req.submit:
         return appKit.sendTransaction(signedTx)
     
-    return ErgoAppKit.unsignedTxToJson(unsignedTx)
+    return appKit.unsignedTxToJson(unsignedTx)
 
 @r.get("/tvl/{tokenId}", name="blockchain:tvl")
 async def tvl(tokenId: str):
